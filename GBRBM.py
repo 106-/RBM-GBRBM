17326768fae3a7d73477afb1d1f2faeb9f7bb2ec
106 <Segmentation-fault@yandex.ua>
2019-11-01 14:15:15 +0900

    勾配のパラメータ化+最適化法の抽象化

--- GBRBM.py
+++ GBRBM.py
@@ -24,6 +24,9 @@ class GBRBM_params(Parameter):
         elif isinstance(initial_params, dict):
             for p in self.param_names:
                 setattr(self, p, initial_params[p])
+        elif isinstance(initial_params, list):
+            for p, i in zip(self.param_names, initial_params):
+                setattr(self, p, i)
         else:
             raise TypeError("initial_params is unknown type :%s"%type(initial_params))
 
@@ -43,58 +46,45 @@ class GBRBM:
     def __init__(self, num_visible, num_hidden):
         self.num_visible = num_visible
         self.num_hidden = num_hidden
-
-        uniform_range = np.sqrt( 6/(num_visible + num_hidden) )
-        self.weight = np.random.uniform(-uniform_range, uniform_range, (num_visible, num_hidden))
-        self.bias_v = np.random.rand(num_visible)
-        self.bias_h = np.random.rand(num_hidden)
-        self.sigma = np.random.randn(num_visible)
+        self.params = GBRBM_params(num_visible, num_hidden)
 
     def _mean_visible(self, hidden_data):
-        return sigmoid( self.bias_v + np.dot( hidden_data, self.weight.T ) )
+        return sigmoid( self.params.bias_v + np.dot( hidden_data, self.params.weight.T ) )
 
     # P(h|v)
     def _prob_h_v(self, visible_data):
-        return sigmoid( self.bias_h + np.dot( visible_data, self.weight ) )
+        return sigmoid( self.params.bias_h + np.dot( visible_data, self.params.weight ) )
 
     def _data_mean(self, visible_data, hidden_data):
         data_length = len(visible_data)
         visible_mean = np.average(visible_data, axis=0) 
         hidden_mean = np.average(hidden_data, axis=0) 
         weight_mean = np.dot(visible_data.T, hidden_data) / data_length
-        sigma_mean = np.average( visible_data / (np.sqrt(2) * self.sigma ** 2), axis=0)
-        return (visible_mean, hidden_mean, weight_mean, sigma_mean)
+        sigma_mean = np.average( visible_data / (np.sqrt(2) * self.params.sigma ** 2), axis=0)
+        return GBRBM_params(self.num_visible, self.num_hidden, initial_params=[weight_mean, visible_mean, hidden_mean, sigma_mean])
 
     def _contrastive_divergence(self, data_prob):
         data_length = len(data_prob)
         hidden_sampling = data_prob > np.random.rand(data_length, self.num_hidden)
-        visible_sampling = self._mean_visible(hidden_sampling) + self.sigma * np.random.randn(data_length, self.num_visible)
+        visible_sampling = self._mean_visible(hidden_sampling) + self.params.sigma * np.random.randn(data_length, self.num_visible)
         return self._data_mean(visible_sampling, data_prob)
 
     def _reconstruct(self, data, gauss=True):
         data_length = len(data)
         hidden_sampling = self._prob_h_v(data) > np.random.rand(data_length, self.num_hidden)
         if gauss: 
-            visible_sampling = self._mean_visible(hidden_sampling) + self.sigma * np.random.randn(data_length, self.num_visible)
+            visible_sampling = self._mean_visible(hidden_sampling) + self.params.sigma * np.random.randn(data_length, self.num_visible)
         else:
             visible_sampling = self._mean_visible(hidden_sampling)
         return visible_sampling
 
-    def train(self, data, learning_rate=0.01, learning_time=1000):
-        for i in range(learning_time):
+    def train(self, data, optimizer, learning_time=1000):
+        for i in range(1, learning_time+1):
             data_prob = self._prob_h_v(data)
-            data_visible, data_hidden, data_weight, data_sigma = self._data_mean(data, data_prob)
-            model_visible, model_hidden, model_weight, model_sigma = self._contrastive_divergence(data_prob)
-
-            grad_v = data_visible - model_visible 
-            grad_h = data_hidden - model_hidden
-            grad_w = data_weight - model_weight
-            grad_sigma = data_sigma - model_sigma
-
-            self.weight += grad_w * learning_rate
-            self.bias_v += grad_v * learning_rate
-            self.bias_h += grad_h * learning_rate
-            self.sigma  += grad_sigma * learning_rate
+            data_exp = self._data_mean(data, data_prob)
+            model_exp = self._contrastive_divergence(data_prob)
+            diff = optimizer.update( model_exp - model_exp )
+            self.params += diff
     
     def recall(self, data, forgotten, gauss=True):
         data[forgotten] = 0
--- train_iris.py
+++ train_iris.py
@@ -4,11 +4,13 @@
 import numpy as np
 from GBRBM import GBRBM
 from sklearn.datasets import load_iris
+from mltools.optimizer import Adamax
 
 def main():
     iris = load_iris()
     gbrbm = GBRBM(4, 3)
-    gbrbm.train(iris.data)
+    adamax = Adamax()
+    gbrbm.train(iris.data, adamax)
 
 if __name__=='__main__':
     main()
